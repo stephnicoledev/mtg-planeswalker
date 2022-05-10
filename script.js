480994d29dc45bdac1c1720a11dfd7f083fbd8b7
iamstephs <stranosteph@gmail.com>
2022-05-10 18:48:11 -0400

    started to add javascript for planeswalkers

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("#jace").addEventListener("click", planeswalkerJace);
+
+function planeswalkerJace() {
+  document.querySelector("h1").innerText = "Jace Beleren";
+  document.querySelector("p").innerText =
+    "Jace Beleren is a human planeswalker and mind-mage from Vryn. He is the former Living Guildpact of Ravnica and a founding member of the Gatewatch.";
+}
